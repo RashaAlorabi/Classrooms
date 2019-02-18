c6b75475120fd2cd4b6bbbadf3c2d54ae329d955
Rasha Alharhi <rasha.alorabi@gmail.com>
2019-02-18 06:06:28 +0300

    classroomProject

--- classes/models.py
+++ classes/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 class Classroom(models.Model):
-	name = models.CharField(max_length=120)
-	subject = models.CharField(max_length=120)
-	year = models.IntegerField()
+    name = models.CharField(max_length=120)
+    teacher = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=120)
+    year = models.IntegerField()
 
-	def get_absolute_url(self):
-		return reverse('classroom-detail', kwargs={'classroom_id':self.id})
+    def get_absolute_url(self):
+        return reverse('classroom-detail', kwargs={'classroom_id':self.id})
+
+class Student(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    name = models.CharField(max_length=120)
+    date_of_birth = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    exam_grade =models.DecimalField(max_digits=6, decimal_places=2)
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
